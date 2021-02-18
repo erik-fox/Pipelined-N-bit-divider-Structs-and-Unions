367da41993d16cef46e4585b5d21b36f0cdbde0b
erik-fox <erikfox589@gmail.com>
2021-02-18 13:57:10 -0800

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -13,48 +13,54 @@ bit [DIVISOR-1:0] divisor;
 wire [DIVIDEND-1:0] quotient;
 wire [DIVISOR-1:0] remainder;
 
+bit [DIVIDEND+DIVISOR-1:0] queue [$];
+bit [DIVIDEND+DIVISOR-1:0] tbcheck;
   
 pipelinediv  #(DIVIDEND,DIVISOR)d0(clock, dividend,divisor,quotient, remainder);
 
 initial
 begin
-clock = FALSE;
-forever #CLOCK_WIDTH clock = ~clock;
+	clock = FALSE;
+	forever #CLOCK_WIDTH clock = ~clock;
 end
   
 initial
 begin
-      $dumpfile("dump.vcd"); $dumpvars;
+	$dumpfile("dump.vcd"); $dumpvars;
 	for(int i=0; i<=(1<<(DIVISOR+DIVIDEND)); i++)
-      	begin
+   	begin
+      		#20 
       		{divisor,dividend}=i;
-        	#20
-		if(quotient[DIVIDEND-1:0]!==(dividend/divisor))
-		begin
-			if(divisor===0)
+    	end
+end
+
+always @(posedge clock)
+begin
+      queue.push_front({divisor,dividend});
+      if(queue.size()===(DIVISOR-1)
+      begin
+      		tbcheck = queue.pop_back();
+      		if(tbcheck[DIVIDEND-1:0]/tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND]!==quotient)
+      		begin
+              		if(tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND]===0)
 				$display("Divide by zero");
          		else
 			begin
 				$display("error");
-         			$display("quotient %d, expected: %d", quotient[DIVIDEND-1:0],(dividend/divisor));
-            			$display("remainder: %d, expected: %d", remainder, (dividend - ((dividend/divisor)*divisor)));
+         			$display("quotient %d, expected: %d", quotient[DIVIDEND-1:0],(tbcheck[DIVIDEND-1:0]/tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND]));
+            			$display("remainder: %d, expected: %d", remainder, (tbcheck[DIVIDEND-1:0] - ((tbcheck[DIVIDEND-1:0]/tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND])*tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND])));
 			end
-		end
-       		else if(remainder!==dividend - (quotient*divisor))
-        	begin
-          		$display("error remainder: %d, expected: %d", remainder, (dividend - ((dividend/divisor)*divisor)));
-        	end
-
+                end
+       		else if(remainder!==tbcheck[DIVIDEND-1:0] - (quotient*tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND]))
+          		$display("error remainder: %d, expected: %d", remainder, (tbcheck[DIVIDEND-1:0] - ((tbcheck[DIVIDEND-1:0]/tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND])*tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND])));
      	 	else
-
-         		$display("dividend: %d divisor: %d quotient: %d remainder %d", dividend, divisor, quotient, remainder);
-
-      	end        
+         		$display("dividend: %d divisor: %d quotient: %d remainder %d", tbcheck[DIVIDEND-1:0], tbcheck[DIVIDEND+DIVISOR-1:DIVIDEND], quotient,remainder);
+         end
 end
-  
+  	
 initial
   begin
-    #2000
+    #1300
     $stop;
   end
 endmodule
