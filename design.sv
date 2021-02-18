cd7486e7867fed78ac8f9f4aed85ce93174760bf
Erik Fox <erfox@auto.ece.pdx.edu>
2021-02-17 18:23:38 -0800

    compiles, ready for debug, NOTE: read through before debugging

--- design.sv
+++ design.sv
@@ -7,32 +7,32 @@ input [DIVISORLEN-1:0]divisor;
 output [DIVIDENDLEN-1:0]quotient;
 output [DIVISORLEN-1:0]remainder;
 
-  wire[DIVIDENDLEN-1:0]w1[DIVIDENDLEN];  //qout
-  wire[DATAPATHLEN-1:0]w2[DIVIDENDLEN];    //dout
-  wire[DIVOSORLEN-1:0]w3[DIVIDENDLEN];     //divout
+wire[DIVIDENDLEN-1:0]w1[DIVIDENDLEN];  //qout
+wire[DATAPATHLEN-1:0]w2[DIVIDENDLEN];    //dout
+wire[DIVISORLEN-1:0]w3[DIVIDENDLEN];     //divout
   
-	bit [DIVISORLEN+DATAPATHLEN+DIVIDENDLEN-1:0]register[DIVIDENDLEN];//bits in divisor, datapath, quotient 
-
+bit [DIVISORLEN+DATAPATHLEN+DIVIDENDLEN-1:0]register[DIVIDENDLEN];//bits in divisor, datapath, quotient 
+bit [DIVIDENDLEN-1:0]q='0;
 genvar i;
 
 generate
-  for (i=0;i<=(DIVIDENDLEN)-1;i++)
+	for (i=0;i<=(DIVIDENDLEN)-1;i++)
 	begin:divider
-        if(i==0)//first
+        	if(i==0)//first
 	    	begin
-          dividerslice #(((DIVIDENDLEN-1)-i),DIVIDENDLEN,DIVISORLEN) d((dividend,divisor,DIVIDENDLEN'b0,w1[i],w2[i],w3[i]);
-		    end
-        else
-        begin
-          dividerslice #(((DIVIDENDLEN-1)-i),DIVIDENDLEN,DIVISORLEN) d((w2[i-1],w3[i-1],w1[i-1],w1[i],w2[i],w3[i]);
-		    end
+          		dividerslice #(((DIVIDENDLEN-1)-i),DIVIDENDLEN,DIVISORLEN) d(dividend,divisor,q,w1[i],w2[i],w3[i]);
+		end
+        	else
+        	begin
+          		dividerslice #(((DIVIDENDLEN-1)-i),DIVIDENDLEN,DIVISORLEN) d(w2[i-1],w3[i-1],w1[i-1],w1[i],w2[i],w3[i]);
+		end
 	end
 endgenerate
 
 always_ff @(posedge clock)
 begin
 	for(int j=0;j<=(DIVIDENDLEN)-1;j++)
-		register<={w1[i],w2[i],w3[i]};
+		register<={w1[j],w2[j],w3[j]};
 end
 assign quotient= w1[DIVIDENDLEN-1];
 assign remainder= w2[DIVIDENDLEN-1];								       
@@ -57,18 +57,18 @@ wire [DATAPATHLEN-1:0] w3;
 
 
 
-  nbitshifter #(SHIFT ,DIVISORLEN,DATAPATHLEN) s0(divin,w1);
-  twoscomplement#(DATAPATHLEN) t0(w1,w2);
-  nbitfulladder #(DATAPATHLEN) a0(din,w2,w3,qout[SHIFT]);
-  mux2_1 #(DATAPATHLEN) m0(din,w3,qout[SHIFT],dout);
+nbitshifter #(SHIFT ,DIVISORLEN,DATAPATHLEN) s0(divin,w1);
+twoscomplement#(DATAPATHLEN) t0(w1,w2);
+nbitfulladder #(DATAPATHLEN) a0(din,w2,w3,qout[SHIFT]);
+mux2_1 #(DATAPATHLEN) m0(din,w3,qout[SHIFT],dout);
   
-  always_comb
-    begin
-      if(SHIFT!=DIVIDENDLEN-1)
-          qout[DIVIDENDLEN-1:SHIFT+1]= qin[DIVIDENDLEN-1:SHIFT+1];
-    end
+always_comb
+	begin
+      		if(SHIFT!=DIVIDENDLEN-1)
+          		qout[DIVIDENDLEN-1:SHIFT+1]= qin[DIVIDENDLEN-1:SHIFT+1];
+    	end
   
-  assign divout = divin;
+assign divout = divin;
   
 endmodule
 
