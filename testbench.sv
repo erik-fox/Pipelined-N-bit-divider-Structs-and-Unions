1ffa0e6cfc1cd91f2d495c72ea889ad18bee727b
erik-fox <erikfox589@gmail.com>
2021-02-18 13:08:25 -0800

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,32 +1,34 @@
 //testbench for pipelined divider
 module top();
-parameter DIVIDEND =3;
+parameter DIVIDEND =4;
 parameter DIVISOR =2;
 parameter TRUE = 1'b1;
 parameter FALSE  = 1'b0;
 parameter CLOCK_CYCLE  = 20;
 parameter CLOCK_WIDTH  = CLOCK_CYCLE/2;
-bit clock;
+ 
+logic clock;
 bit [DIVIDEND-1:0] dividend;
 bit [DIVISOR-1:0] divisor;
 wire [DIVIDEND-1:0] quotient;
 wire [DIVISOR-1:0] remainder;
+
   
-pipelinediv  #(DIVIDEND,DIVISOR)d0(clock,dividend,divisor,quotient, remainder);
+pipelinediv  #(DIVIDEND,DIVISOR)d0(clock, dividend,divisor,quotient, remainder);
 
 initial
 begin
 clock = FALSE;
 forever #CLOCK_WIDTH clock = ~clock;
 end
-	
+  
 initial
 begin
       $dumpfile("dump.vcd"); $dumpvars;
 	for(int i=0; i<=(1<<(DIVISOR+DIVIDEND)); i++)
       	begin
       		{divisor,dividend}=i;
-        	#10
+        	#20
 		if(quotient[DIVIDEND-1:0]!==(dividend/divisor))
 		begin
 			if(divisor===0)
@@ -42,17 +44,17 @@ begin
         	begin
           		$display("error remainder: %d, expected: %d", remainder, (dividend - ((dividend/divisor)*divisor)));
         	end
-`ifdef DEBUG
+
      	 	else
 
          		$display("dividend: %d divisor: %d quotient: %d remainder %d", dividend, divisor, quotient, remainder);
-`endif
+
       	end        
 end
-
+  
 initial
-begin 	
-	#1000
-	$stop;
-end  
+  begin
+    #2000
+    $stop;
+  end
 endmodule
