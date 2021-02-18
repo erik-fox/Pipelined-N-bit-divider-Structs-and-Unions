597b2f90db57f451b63302cf908d528f38059b06
erik-fox <erikfox589@gmail.com>
2021-02-18 12:01:53 -0800

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,27 +1,25 @@
 //testbench for pipelined divider
 module top();
-parameter DIVIDEND =3;
-parameter DIVISOR =2;
+parameter DIVIDEND =16;
+parameter DIVISOR =8;
 parameter TRUE = 1'b1;
 parameter FALSE  = 1'b0;
 parameter CLOCK_CYCLE  = 20;
 parameter CLOCK_WIDTH  = CLOCK_CYCLE/2;
- 
-logic clock;
+bit clock;
 bit [DIVIDEND-1:0] dividend;
 bit [DIVISOR-1:0] divisor;
 wire [DIVIDEND-1:0] quotient;
 wire [DIVISOR-1:0] remainder;
-
   
-pipelinediv  #(DIVIDEND,DIVISOR) d0(clock, dividend,divisor,quotient, remainder);
+divider  #(DIVIDEND,DIVISOR)d0(dividend,divisor,quotient, remainder);
 
 initial
 begin
 clock = FALSE;
 forever #CLOCK_WIDTH clock = ~clock;
 end
-  
+	
 initial
 begin
       $dumpfile("dump.vcd"); $dumpvars;
@@ -44,11 +42,11 @@ begin
         	begin
           		$display("error remainder: %d, expected: %d", remainder, (dividend - ((dividend/divisor)*divisor)));
         	end
-
+`ifdef DEBUG
      	 	else
 
          		$display("dividend: %d divisor: %d quotient: %d remainder %d", dividend, divisor, quotient, remainder);
-
+`endif
       	end        
 end
   
