9c471db119f83d1cc8f7d7b379fb384df9d23f81
erik-fox <erikfox589@gmail.com>
2021-02-18 20:01:00 -0800

    Update mipstest.sv

--- mipstest.sv
+++ mipstest.sv
@@ -9,10 +9,12 @@ initial
 begin
   $dumpfile("dump.vcd"); $dumpvars;
   #10
-  $display("r - opcode: %b rs: %b rt: %b rd: %b shift: %b func: %b",rinstr.r.opcode,rinstr.r.rs, rinstr.r.rt,rinstr.r.rd,rinstr.r.shift,rinstr.r.func);
+  $display("r  - opcode: %b rs: %b rt: %b rd: %b shift: %b func: %b",rinstr.r.opcode,rinstr.r.rs, rinstr.r.rt,rinstr.r.rd,rinstr.r.shift,rinstr.r.func);
+  $display("expected - 000000 10001 10010 10000 00000 100000");
   $display("i - opcode: %b rs: %b rt: %b imm: %b",iinstr.i.opcode, iinstr.i.rs,iinstr.i.rt, iinstr.i.imm);
+  $display("expected - 001000 10011 01010 0000000000000100");
   $display("j - opcode: %b address: %b", jinstr.j.opcode, jinstr.j.address);
-  
+  $display("expected - 000010 00000000000000000100000001");
 end
   
   
