f2d0417f3555347bb3c7fa29f673b61adab53dc9
erik-fox <erikfox589@gmail.com>
2021-02-18 18:55:50 -0800

    Update mipstest.sv

--- mipstest.sv
+++ mipstest.sv
@@ -1,1 +1,18 @@
-
+module top();
+  
+import mips::*;
+  mipsinst instr=32'h87654321;
+  
+initial
+begin
+  $dumpfile("dump.vcd"); $dumpvars;
+  #10
+  $display("r - opcode: %b rs: %b rt: %b rd: %b shift: %b func: %b",instr.r.opcode,instr.r.rs, instr.r.rt,instr.r.rd,instr.r.shift,instr.r.func);
+  $display("i - opcode: %b rs: %b rt: %b imm: %b",instr.i.opcode, instr.i.rs,instr.i.rt, instr.i.imm);
+  $display("j - opcode: %b address: %b", instr.j.opcode, instr.j.address);
+  
+end
+  
+  
+  
+endmodule
