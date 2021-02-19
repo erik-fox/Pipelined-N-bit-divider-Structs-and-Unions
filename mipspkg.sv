533976f27acba3025a0a70c0b8f267f32faf8af4
erik-fox <erikfox589@gmail.com>
2021-02-18 18:38:18 -0800

    Update mipspkg.sv

--- mipspkg.sv
+++ mipspkg.sv
@@ -2,10 +2,25 @@ package mips;
   
   typedef union packed{
     struct packed {
+      bit [31:26]opcode;//6bits
+      bit [25:21]rs;//5bits
+      bit [20:16]rt;//5bits
+      bit [15:11]rd;//5bits
+      bit [10:6]shift;//5bits
+      bit [5:0]func;//6bits
     }r;
     struct packed {
+      bit[31:26]opcode;//6bits
+      bit [25:21]rs;//5bits
+      bit [20:16]rt;//5bits
+      bit [15:0]imm;//16bits
     }i;
     struct packed {
-    }i;
+      bit [31:26]opcode;
+      bit [25:0]address;
+    }j;
   }mipsinst;
+  
+  
+  
 endpackage
