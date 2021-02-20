1ead21c52105f4c4a5ef4b8646553599d55468ac
erik-fox <erikfox589@gmail.com>
2021-02-20 10:54:18 -0800

    Update tcptest.sv

--- tcptest.sv
+++ tcptest.sv
@@ -1,7 +1,20 @@
-task sendByte();
-
+import tcp::*;
+module top();
+  
+  initial
+    begin
+      
+    end
+endmodule
+               
+task sendByte(bit [7:0] din);
+  $display("Byte: %b",din);
 endtask
 
-task sendSegmentHeader();
-
+task sendSegmentHeader(tcpheader din);
+  for(int i =0; i<= din.offset*4;i++)
+    sendByte(din[480-1-(i*8)-:8];
 endtask
+
+             
+            
