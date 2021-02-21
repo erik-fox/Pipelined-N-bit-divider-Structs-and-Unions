49783e0a18d4d32762be695fcaf953642e85f557
Erik Fox <erfox@auto.ece.pdx.edu>
2021-02-21 14:09:18 -0800

    ready to submit

--- tcptest.sv
+++ tcptest.sv
@@ -1,14 +1,25 @@
 module top();
 import tcp::*;
+
 task sendByte(bit [7:0] din);
   $display("Byte: %h",din);
 endtask
 
+`ifdef STREAM
+task sendSegmentHeader(tcpheader din);
+  bit [7:0] stream [$];
+  stream = {>>{din}};
+  for(int i =0; i<= (din.offset * 4) - 1; i++)
+      sendByte(stream.pop_front);
+endtask
+
+`else
 task sendSegmentHeader(tcpheader din);
   for(int i =0; i<= (din.offset * 4) - 1; i++)
     sendByte(din[480-1-(i*8)-:8]);
 endtask
 
+`endif
 tcpheader data;
   initial
     begin
