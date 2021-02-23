31965aeaf9e31a332ba4384fe54b11d3754db831
erik-fox <erikfox589@gmail.com>
2021-02-22 18:05:23 -0800

    Update mipstest.sv

--- mipstest.sv
+++ mipstest.sv
@@ -4,17 +4,24 @@ import mips::*;
 mipsinst iinstr=32'b00100010011010100000000000000100;
 mipsinst jinstr=32'b00001000000000000000000100000001;
 mipsinst rinstr=32'b00000010001100101000000000100000;
+mipsinst instr[3]={iinstr,jinstr,rstr};
   
 initial
 begin
   $dumpfile("dump.vcd"); $dumpvars;
   #10
-  $display("r  - opcode: %b rs: %b rt: %b rd: %b shift: %b func: %b",rinstr.r.opcode,rinstr.r.rs, rinstr.r.rt,rinstr.r.rd,rinstr.r.shift,rinstr.r.func);
-  $display("expected - 000000 10001 10010 10000 00000 100000");
-  $display("i - opcode: %b rs: %b rt: %b imm: %b",iinstr.i.opcode, iinstr.i.rs,iinstr.i.rt, iinstr.i.imm);
-  $display("expected - 001000 10011 01010 0000000000000100");
-  $display("j - opcode: %b address: %b", jinstr.j.opcode, jinstr.j.address);
-  $display("expected - 000010 00000000000000000100000001");
+  for(int i=0;i<=3; i++}
+      case(instr[i].r.opcode)
+        6'b000000: $display("r  - opcode: %b rs: %b rt: %b rd: %b shift: %b func: %b",instr[i].r.opcode,instr[i].r.rs, instr[i].r.rt,instr[i].r.rd,instr[i].r.shift,instr[i].r.func);
+        6'b000010,6'b000011 :  $display("j - opcode: %b address: %b", instr[i].j.opcode, instr[i].j.address);
+        default: $display("i - opcode: %b rs: %b rt: %b imm: %b",instr[i].i.opcode, instr[i].i.rs,instr[i].i.rt, instr[i].i.imm);
+      endcase
+  
+
+  
+
+
+
 end
   
   
